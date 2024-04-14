8226555d978543b45dda876aa62c72f21057cfda
Maximiliano Tis <maxi_tis@hotmail.com>
2024-04-14 17:52:59 -0300

    Inicio del proyecto: Buscador de peliculas con Javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('searchButton').addEventListener('click',searchMovies)
+
+function searchMovies(){
+    let searchInput = document.getElementById('searchInput').value
+    
+}
